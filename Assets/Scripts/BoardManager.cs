e27bfe0a278634ca01f11ffe506937456109b822
Abadd0n <lpytlak27@gmail.com>
2019-06-03 14:49:40 +0200

    First commit with GameBoard and Dragon balls spawn and Prevent repeating tiles mechanism, no select and swap yet.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardManager : MonoBehaviour
+{
+
+
+    public static BoardManager Instance;
+
+    public GameObject Tile;
+    public GameObject Parent;
+
+    public List<GameObject> Balls = new List<GameObject>();
+    public int axisX, axisY;
+    
+    private GameObject[,] Tiles;
+    public bool IsShifting { get; set; }
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Instance = GetComponent<BoardManager>();
+        CreateBoard();
+	}
+	
+    // Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+    private void CreateBoard()
+    {
+        Tiles = new GameObject[axisX, axisY];
+
+        GameObject[] previousLeft = new GameObject[axisY];
+        GameObject previousBelow = null;
+
+        for (int i = 0; i < axisX; i++)
+        {
+            for (int j = 0; j < axisY; j++)
+            {
+                GameObject newTile = Instantiate(Tile, new Vector3(i, j, 0),
+                    Quaternion.identity);
+
+                newTile.transform.SetParent(Parent.transform); 
+
+                Tiles[i, j] = newTile;
+
+                List<GameObject> possibleCharacters = new List<GameObject>(); // 1
+                possibleCharacters.AddRange(Balls); // 2
+
+                possibleCharacters.Remove(previousLeft[j]); // 3
+                possibleCharacters.Remove(previousBelow);
+
+                Tile = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+
+                previousLeft[j] = Tile;
+                previousBelow = Tile;
+            }
+        }
+
+    }
+
+    private void OnMouseClick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+	//private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Tile previousSelected = null;
+    private Renderer rend;
+    private GameObject go;
+	private bool isSelected = false;
+
+	//private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	void Awake() {
+		go = GetComponent<GameObject>();
+    }
+
+	private void Select() {
+		isSelected = true;
+		//go. = selectedColor;
+		previousSelected = gameObject.GetComponent<Tile>();
+		//SFXManager.instance.PlaySFX(Clip.Select);
+	}
+
+	private void Deselect() {
+		isSelected = false;
+		//go.color = Color.white;
+		previousSelected = null;
+	}
+    void OnMouseDown()
+    {
+        // 1
+        if (go == null || BoardManager.Instance.IsShifting)
+        {
+            Debug.Log(BoardManager.Instance.IsShifting.Equals(true));
+            return;
+        }
+
+        if (isSelected)
+        { // 2 Is it already selected?
+            Deselect();
+        }
+        else
+        {
+            if (previousSelected == null)
+            { // 3 Is it the first tile selected?
+                Debug.Log(BoardManager.Instance.IsShifting.Equals(true));
+                Select();
+                Debug.Log(BoardManager.Instance.IsShifting.Equals(true));
+            }
+            else
+            {
+                Debug.Log(BoardManager.Instance.IsShifting.Equals(true));
+                previousSelected.Deselect(); // 4
+                Debug.Log(BoardManager.Instance.IsShifting.Equals(true));
+            }
+        }
+    }
+}
